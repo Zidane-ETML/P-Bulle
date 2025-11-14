da627d4ad404ce24fe629765394fcef19db5d3ad
Zidane-ETML <pd01wvj@eduvaud.ch>
2025-11-14 16:12:26 +0100

    fix(annalyns-infiltration): Recorrection car j'ai oublié un endroit [1][DONE]

--- exercices/annalyns-infiltration/annalyns-infiltration.js
+++ exercices/annalyns-infiltration/annalyns-infiltration.js
@@ -27,7 +27,7 @@
  * @return {boolean} Whether or not you can execute a fast attack.
  */
 export function canExecuteFastAttack(knightIsAwake) {
-  return knightIsAwake == false
+  return !knightIsAwake
 }
 
 /**
