c5715b8c7cc33be345d43a81a00020eb372a3bce
Zidane-ETML <pd01wvj@eduvaud.ch>
2025-11-14 15:35:08 +0100

    feat(door-policy): Réalisation de l'exercice [30][DONE]

--- exercices/poetry-club-door-policy/door-policy.js
+++ exercices/poetry-club-door-policy/door-policy.js
@@ -27,7 +27,7 @@
  * @returns {string}
  */
 export function frontDoorResponse(line) {
-  throw new Error('Remove this line and implement the function');
+  return line[0]
 }
 
 /**
@@ -38,7 +38,9 @@ export function frontDoorResponse(line) {
  * @returns {string} the front door password
  */
 export function frontDoorPassword(word) {
-  throw new Error('Remove this line and implement the function');
+  const letter = word[0].toUpperCase()
+  const loword = word.toLowerCase()
+  return letter + loword.slice(1)
 }
 
 /**
@@ -49,7 +51,8 @@ export function frontDoorPassword(word) {
  * @returns {string}
  */
 export function backDoorResponse(line) {
-  throw new Error('Remove this line and implement the function');
+  const lineTrim = line.trim()
+  return lineTrim[lineTrim.length-1]
 }
 
 /**
@@ -60,5 +63,5 @@ export function backDoorResponse(line) {
  * @returns {string} the back door password
  */
 export function backDoorPassword(word) {
-  throw new Error('Remove this line and implement the function');
+  return frontDoorPassword(word) + ", please"
 }
