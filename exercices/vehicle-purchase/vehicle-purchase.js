0ba09ab4d56aa8a127c89ddd35d5337952815c95
SHR-Zidane <pd01wvj@eduvaud.ch>
2025-11-21 15:14:51 +0100

    fix(vehicle-purchase): amélioration du code pour qu'il soit plus lisible (opérateur ternaire utilisé) [20][DONE]

--- exercices/vehicle-purchase/vehicle-purchase.js
+++ exercices/vehicle-purchase/vehicle-purchase.js
@@ -23,11 +23,7 @@ export function needsLicense(kind) {
  * @returns {string} a sentence of advice which option to choose
  */
 export function chooseVehicle(option1, option2) {
-  if (option1 < option2){
-    return option1 + " is clearly the better choice."
-  }
-  else 
-    return option2 + " is clearly the better choice."
+  return (option1 < option2) ? option1 + " is clearly the better choice." : option2 + " is clearly the better choice.";
    
 }
 
@@ -40,14 +36,5 @@ export function chooseVehicle(option1, option2) {
  * @returns {number} expected resell price in the dealership
  */
 export function calculateResellPrice(originalPrice, age) {
-  if (age < 3)
-    {
-      return originalPrice * 0.8
-    }
-  else if (age > 10)
-    {
-      return originalPrice * 0.5
-    }
-  else
-    return originalPrice * 0.7
+  return age < 3 ? originalPrice*0.8 : age > 10 ? originalPrice*0.5 : originalPrice*0.7;
 }
